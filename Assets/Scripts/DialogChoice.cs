467f5cb38ed59bb079f903c2a80ce789b9111b6d
Sam Trapp <trapp.s.e@gmail.com>
2014-07-08 23:40:43 -0400

    Basic text dumping.

---
+++ Assets/Scripts/DialogChoice.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogChoice : MonoBehaviour {
+	public GUIText keyDisplay;
+	private char choiceKey;
+	public char ChoiceIndex {
+		get { return choiceKey; }
+		set {
+			choiceKey = value;
+			keyDisplay.text = choiceKey.ToString();
+		}
+	}
+	public GUIText dumpDisplay;
+	private TextDump dump;
+	public TextDump Dump {
+		get { return dump; }
+		set {
+			dump = value; 
+			if (dump == null || dump.text == null) {
+				keyDisplay.enabled = false;
+				dumpDisplay.enabled = false;
+			} else {
+				keyDisplay.enabled = true;
+				dumpDisplay.enabled = true;
+				dumpDisplay.text = dump.text;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TextDumper.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TextDumper : MonoBehaviour {
+	public List<TextDump> dumps;
+	private List<TextDump> potentialDumps;
+	public List<DialogChoice> choices; 
+	private System.Random random;
+	public int seed;
+
+	void Start() {
+		if (seed <= 0) {
+			seed = (int)System.DateTime.Now.Ticks;
+		}
+		random = new System.Random(seed);
+
+		// TODO This should be handled conditionally elsewhere, potentialDumps should be a subset
+		potentialDumps = new List<TextDump>();
+		for (int i = 1; i < dumps.Count; i++) {
+			potentialDumps.Add(dumps[i]);
+		}
+
+		for (int i = 0; i < choices.Count; i++) {
+			choices[i].ChoiceIndex = i.ToString()[0];
+		}
+
+		UpdateAllChoices();
+	}
+
+	void Update() {
+		for (int i = 0; i < choices.Count; i++) {
+			if (choices[i].dumpDisplay.enabled && Input.GetKeyDown(choices[i].ChoiceIndex.ToString())) {
+				Debug.Log(choices[i].ChoiceIndex);
+			}
+		}
+	}
+
+	private void UpdateChoice(int choiceIndex) {
+		if (potentialDumps.Count < 1) {
+			choices[choiceIndex].Dump = null;
+		} else {
+			int dumpIndex;
+			dumpIndex = random.Next(0, potentialDumps.Count);
+			choices[choiceIndex].Dump = potentialDumps[dumpIndex];
+			potentialDumps.RemoveAt(dumpIndex);
+		}
+	}
+
+	private void UpdateAllChoices() {
+		// Zeroth choice is default.
+		choices[0].Dump = dumps[0];
+
+		for (int i = 1; i < choices.Count; i++) {
+			UpdateChoice(i);
+		}
+	}
+}
+
+[System.Serializable]
+public class TextDump {
+	public string text;
+}
