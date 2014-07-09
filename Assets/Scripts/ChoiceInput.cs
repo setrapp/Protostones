6ff7bfaec2948afe1e8efc9fc5567ddb2b6a2675
Sam Trapp <trapp.s.e@gmail.com>
2014-07-09 11:27:50 -0400

    Choices reset after making one. Can have multiple guaranteed text dumps.

---
+++ Assets/Scripts/ChoiceInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChoiceInput : MonoBehaviour {
+	public TextDumper dumper;
+
+	void Update () {
+		List<DialogChoice> choices = dumper.choices;
+		for (int i = 0; i < choices.Count; i++) {
+			if (choices[i].dumpDisplay.enabled && Input.GetKeyDown(choices[i].ChoiceIndex.ToString())) {
+				dumper.DumpText(choices[i].Dump);
+				break;
+			}
+		}
+	}
+}
--- Assets/Scripts/TextDumper.cs
+++ Assets/Scripts/TextDumper.cs
@@ -8,6 +8,8 @@ public class TextDumper : MonoBehaviour {
 	public List<DialogChoice> choices; 
 	private System.Random random;
 	public int seed;
+	private int guaranteedDumps;
+	public GUIText dumpDisplay;
 
 	void Start() {
 		if (seed <= 0) {
@@ -15,44 +17,64 @@ public class TextDumper : MonoBehaviour {
 		}
 		random = new System.Random(seed);
 
-		// TODO This should be handled conditionally elsewhere, potentialDumps should be a subset
 		potentialDumps = new List<TextDump>();
-		for (int i = 1; i < dumps.Count; i++) {
-			potentialDumps.Add(dumps[i]);
-		}
+		ResetPotentialDumps();
 
 		for (int i = 0; i < choices.Count; i++) {
 			choices[i].ChoiceIndex = i.ToString()[0];
 		}
 
+		guaranteedDumps = 1;
 		UpdateAllChoices();
 	}
 
-	void Update() {
-		for (int i = 0; i < choices.Count; i++) {
-			if (choices[i].dumpDisplay.enabled && Input.GetKeyDown(choices[i].ChoiceIndex.ToString())) {
-				Debug.Log(choices[i].ChoiceIndex);
+	private void UpdateChoice(int choiceIndex, int dumpIndex) {
+		choices[choiceIndex].Dump = potentialDumps[dumpIndex];
+		potentialDumps.RemoveAt(dumpIndex);
+	}
+
+	private void UpdateAllChoices() {
+		for (int i = 0; i < guaranteedDumps; i++) {
+			if (potentialDumps.Count > 0 && i < choices.Count) {
+				UpdateChoice(i, 0);
+			} else {
+				choices[i].Dump = null;
+			}
+		}
+
+		for (int i = guaranteedDumps; i < choices.Count; i++) {
+			if (potentialDumps.Count > 0) {
+				int dumpIndex;
+				dumpIndex = random.Next(0, potentialDumps.Count);
+				UpdateChoice(i, dumpIndex);
+			} else {
+				choices[i].Dump = null;
 			}
 		}
 	}
 
-	private void UpdateChoice(int choiceIndex) {
-		if (potentialDumps.Count < 1) {
-			choices[choiceIndex].Dump = null;
-		} else {
-			int dumpIndex;
-			dumpIndex = random.Next(0, potentialDumps.Count);
-			choices[choiceIndex].Dump = potentialDumps[dumpIndex];
-			potentialDumps.RemoveAt(dumpIndex);
+	public bool DumpText(TextDump dump) {
+		if (!dumps.Contains(dump)) {
+			return false;
 		}
+
+		dumpDisplay.enabled = true;
+		dumpDisplay.text = dump.text;
+
+		ResetPotentialDumps();
+
+		// TODO might not need to happen here
+		UpdateAllChoices();
+
+		return true;
 	}
 
-	private void UpdateAllChoices() {
-		// Zeroth choice is default.
-		choices[0].Dump = dumps[0];
-
-		for (int i = 1; i < choices.Count; i++) {
-			UpdateChoice(i);
+	private void ResetPotentialDumps() {
+		potentialDumps.Clear();
+		for (int i = 0; i < dumps.Count; i++) {
+			if (dumps[i].isCommon) {
+				potentialDumps.Add(dumps[i]);
+			}
 		}
 	}
 }
@@ -60,4 +82,5 @@ public class TextDumper : MonoBehaviour {
 [System.Serializable]
 public class TextDump {
 	public string text;
+	public bool isCommon;
 }
