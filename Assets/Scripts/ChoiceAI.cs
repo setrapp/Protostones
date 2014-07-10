193556ac0d5b30a676dccbe794e012a4b594bb2a
Sam Trapp <trapp.s.e@gmail.com>
2014-07-10 17:13:36 -0400

    Starting to add affinity to text dumps.

--- Assets/Scripts/ChoiceAI.cs
+++ Assets/Scripts/ChoiceAI.cs
@@ -6,16 +6,29 @@ public class ChoiceAI : MonoBehaviour {
 	public TextDumper dumper;
 	private System.Random random;
 	public int seed;
+	public List<DumpAffinity> dumpAffinities;
+	public float liking;
 
 	void Start() {
 		if (seed <= 0) {
 			seed = (int)System.DateTime.Now.Ticks;
 		}
 		random = new System.Random(seed);
+
+		// Attach affinities to partners dumps.
+		List<TextDump> partnerDumps = dumper.partner.dumps;
+		List<TextDump> partnerResponses = dumper.partner.responses;
+		for (int i = 0; i < partnerDumps.Count; i++) {
+			dumpAffinities[i].dump = partnerDumps[i];
+		}
+		int startingIndex = partnerDumps.Count;
+		for (int i = startingIndex; i < partnerResponses.Count + startingIndex; i++) {
+			dumpAffinities[i].dump = partnerResponses[i - startingIndex];
+		}
 	}
 
 	void Update() {
-		/* TODO this should not be player controlled*/
+		// TODO this should not be player controlled
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			RandomDump();
 		}
@@ -31,4 +44,30 @@ public class ChoiceAI : MonoBehaviour {
 		int dumpIndex = random.Next(0, validChoices.Count);
 		dumper.DumpText(validChoices[dumpIndex].Dump);
 	}
+
+	void RespondToDump(TextDump dump) {
+		bool dumpFound = false;
+		for (int i = 0; i < dumpAffinities.Count && !dumpFound; i++) {
+			if (dumpAffinities[i].dump == dump) {
+				dumpFound = true;
+				if (liking >= dumpAffinities[i].minToLike && liking <= dumpAffinities[i].minToLike) {
+					liking += dumpAffinities[i].likeChange;
+				} else if (liking <= dumpAffinities[i].maxToDislike && liking <= dumpAffinities[i].maxToDislike) {
+					liking -= dumpAffinities[i].dislikeChange;
+				}
+			}
+		}
+	}
+}
+
+[System.Serializable]
+public class DumpAffinity {
+	public TextDump dump;
+	public string dumpName; // Just need to see name in editor
+	public float minToLike;
+	public float maxToLike;
+	public float likeChange;
+	public float minToDislike;
+	public float maxToDislike;
+	public float dislikeChange;
 }
