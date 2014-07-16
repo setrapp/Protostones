879e61745b9141b284b47ff8bde42fd0cb6ecaa2
Sam Trapp <trapp.s.e@gmail.com>
2014-07-16 10:51:33 -0400

    Added basic movement and terrible camera control.

---
+++ Assets/Scripts/SimpleMotor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMotor : MonoBehaviour {
+	public float moveSpeed;
+	public Vector3 movement;
+	private CharacterController controller;
+
+	void Start() {
+		controller = GetComponent<CharacterController>();
+	}
+
+	public void UpdateMotor() {
+		//AlignWithCameraSnap();
+		ProcessMotion();
+	}
+
+	private void AlignWithCameraSnap() {
+		if (movement.sqrMagnitude > 0) 
+		{
+			Vector3 rotation = transform.rotation.eulerAngles;
+			rotation.y = Camera.main.transform.rotation.y;
+			transform.rotation = Quaternion.Euler(rotation);
+		}
+	}
+
+	private void ProcessMotion() {
+		Vector3 worldMovement = transform.TransformDirection(movement);
+		if (worldMovement.sqrMagnitude > 1) {
+			worldMovement.Normalize();
+		}
+		worldMovement *= moveSpeed * Time.deltaTime;
+		controller.Move (worldMovement);
+	}
+}
---
+++ Assets/Scripts/SimpleMoveInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMoveInput : MonoBehaviour {
+	public float deadSpace;
+	private SimpleMotor motor;
+
+	void Start() {
+		motor = GetComponent<SimpleMotor>();
+	}
+
+	void Update() {
+		motor.movement = Vector3.zero;
+		if (Mathf.Abs(Input.GetAxis("Vertical")) > deadSpace) {
+			motor.movement += Vector3.forward * Input.GetAxis("Vertical");
+		}
+		if (Mathf.Abs(Input.GetAxis("Horizontal")) > deadSpace) {
+			motor.movement += Vector3.right * Input.GetAxis("Horizontal");
+		}
+
+		motor.UpdateMotor();
+	}
+}
