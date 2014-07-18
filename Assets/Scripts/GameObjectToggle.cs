28a82007aeb9c9396bce44b4fa207f430f602878
Sam Trapp <trapp.s.e@gmail.com>
2014-07-18 11:11:09 -0400

    Fixed layer mask issue and allowed multiple layers to be toggles in GameObjectToggle.

--- Assets/Scripts/GameObjectToggle.cs
+++ Assets/Scripts/GameObjectToggle.cs
@@ -5,30 +5,12 @@ public class GameObjectToggle : MonoBehaviour {
 	public GameObject target;
 	public LayerMask layerOn;
 	public LayerMask layerOff;
-	private int layerOnIndex = -1;
-	private int layerOffIndex= -1;
-
-	void Start() {
-		// Convert layermask value to power of 2 index. 
-		// Is there not an automatic way of doing this???
-		int value = 1;
-		int pow = 0;
-		do {
-			if (value == layerOn.value) {
-				layerOnIndex = pow;
-			}
-			if (value == layerOff.value) {
-				layerOffIndex = pow;
-			}
-			value *= 2;
-			pow++;
-		} while (pow < 32 && (layerOnIndex < 0 || layerOffIndex < 0));
-	}
 
 	void OnTriggerEnter(Collider collider) {
-		if (collider.gameObject.layer == layerOnIndex) {
+		int layerValue = (int)Mathf.Pow(2, collider.gameObject.layer);
+		if ((layerValue & layerOn.value) == layerValue) {
 			target.SetActive(true);
-		} else if (collider.gameObject.layer == layerOffIndex) {
+		} else if ((layerValue & layerOff.value) == layerValue) {
 			target.SetActive(false);
 		}
 	}
