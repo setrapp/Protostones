40264a26349139b9b285e23ac97aeed2a88a7fe7
Sam Trapp <trapp.s.e@gmail.com>
2014-07-18 13:08:20 -0400

    Created region terrain and updated toggles to avoid multiple toggles at once.

--- Assets/Scripts/GameObjectToggle.cs
+++ Assets/Scripts/GameObjectToggle.cs
@@ -5,13 +5,23 @@ public class GameObjectToggle : MonoBehaviour {
 	public GameObject target;
 	public LayerMask layerOn;
 	public LayerMask layerOff;
+	private GameObject collidingObject = null;
+	private bool toggleOn;
 
 	void OnTriggerEnter(Collider collider) {
 		int layerValue = (int)Mathf.Pow(2, collider.gameObject.layer);
-		if ((layerValue & layerOn.value) == layerValue) {
-			target.SetActive(true);
-		} else if ((layerValue & layerOff.value) == layerValue) {
-			target.SetActive(false);
+		if ((layerValue & layerOn.value) == layerValue && !target.activeSelf) {
+			collidingObject = collider.gameObject;
+			toggleOn = true;
+		} else if ((layerValue & layerOff.value) == layerValue && target.activeSelf) {
+			collidingObject = collider.gameObject;
+			toggleOn = false;
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		if (collider.gameObject == collidingObject) {
+			target.SetActive(toggleOn);
 		}
 	}
 }
